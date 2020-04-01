0cf3aa92e9dbccc6b0fc083ce29f2df9f2695150
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2020-04-01 10:38:17 +0200

    add google analytics and tag manager
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/media/ahmedaabouzied/D/code/work/ahmedaabouzied.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":1000,"quality":80,"showCaptions":true,"linkImagesToOriginal":false},
-    },{
-      plugin: require('/media/ahmedaabouzied/D/code/work/ahmedaabouzied.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
