6f5bcd91989403c4e6506f80c8888b34769720e6
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2020-04-02 13:09:40 +0200

    update google analytics config
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -72,7 +72,6 @@ module.exports = [
       // Enables Google Optimize Experiment ID
       experimentId: "lsZSqACSTA-jAlZRuAjkEQ",
       // Set Variation ID. 0 for original 1,2,3....
-      variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
       // Any additional optional fields
       sampleRate: 5,
       siteSpeedSampleRate: 10,
@@ -91,12 +90,7 @@ module.exports = [
       // datalayer to be set before GTM is loaded
       // should be an object or a function that is executed in the browser
       // Defaults to null
-      defaultDataLayer: { platform: "gatsby" },
-
-      // Specify optional GTM environment details.
-      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-      gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-      dataLayerName: "YOUR_DATA_LAYER_NAME"
+      defaultDataLayer: { platform: "gatsby" }
     }
   }
 ];
